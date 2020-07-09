9e15f18a7cea61e971aa72c1c7b891c89dc75e0d
richmondvan <evan.ty.zheng@gmail.com>
2020-07-08 22:51:59 -0400

    - Miscellaneous refactorings.

--- src/circlevolve/callbacks.py
+++ src/circlevolve/callbacks.py
@@ -3,26 +3,26 @@ from utils import show_image
 display = None
 
 
-def default_callback(offspring, changes, generation_index, total_generation_changes, top_score,
-                     extreme_mutation, top_image, adjusters):
+def default_callback(offspring, changes, loop_index, num_mutation_type_switches, error,
+                     complex_mutation, best_image, adjusters):
     global display
 
-    print(f"offspring: {offspring}")
-    print(f"number of changes: {changes}")
-    print(f"number of total generations: {generation_index}")
-    print(f"number of evolution switches: {total_generation_changes}")
-    print(f"error: {top_score}")
-    extreme = "extreme" if extreme_mutation else "not extreme"
-    print(f"Extreme?: {extreme}")
+    print(f"Total offspring: {offspring}")
+    print(f"Total changes: {changes}")
+    print(f"Total loop iterations: {loop_index}")
+    print(f"Total mutation type switches: {num_mutation_type_switches}")
+    print(f"Current error: {error}")
+    extreme = "Yes" if complex_mutation else "No"
+    print(f"Is currently in a complex mutation: {extreme}")
 
     if display is None:
-        display = show_image(top_image, adjusters=adjusters)
+        display = show_image(best_image, adjusters=adjusters)
     else:
-        show_image(top_image, display=display, adjusters=adjusters)
+        show_image(best_image, display=display, adjusters=adjusters)
 
 
-def quiet_callback(offspring, changes, generation_index, total_generation_changes, top_score,
-                   extreme_mutation, top_image, adjusters):
+def quiet_callback(offspring, changes, loop_index, num_mutation_type_switches, error,
+                   complex_mutation, best_image, adjusters):
     if changes % 50 == 0:
-        default_callback(offspring, changes, generation_index, total_generation_changes, top_score,
-                         extreme_mutation, top_image, adjusters)
+        default_callback(offspring, changes, loop_index, num_mutation_type_switches, error,
+                         complex_mutation, best_image, adjusters)
--- src/circlevolve/evolver.py
+++ src/circlevolve/evolver.py
@@ -44,7 +44,6 @@ class Evolver:
         self.calculate_error = calculate_error
 
         loaded_image = Image.open(filepath)
-
         for preprocess in preprocesses:
             loaded_image = preprocess(loaded_image)
 
@@ -52,7 +51,6 @@ class Evolver:
         self.width = int(loaded_image.width * self.ratio)
         self.height = int(loaded_image.height * self.ratio)
         loaded_image = loaded_image.resize((self.width, self.height), Image.LANCZOS)
-
         image_array = adjust(np.asarray(loaded_image), adjusters)
 
         loaded_image = Image.fromarray(image_array)
@@ -93,33 +91,33 @@ class Evolver:
         cached_image = None  # Location to store a cached image for later use.
         recall_from_cache = False  # Whether to load an image from the cache or build it from scratch.
 
-        generations_since_last_change = 0  # counter for above.
-        total_generation_changes = 0  # number of total generation changes,
+        num_consecutive_failed_loops = 0  # counter for above.
+        num_mutation_type_switches = 0  # number of total generation changes,
         # used to determine how long an algorithm should stay complex.
-        consecutive_extreme_mutations = 0  # counter for early stopping.
-        extreme_mutation = False  # default value of whether to use extreme mutations.
+        num_complex_mutation_successes_since_switch = 0  # counter for early stopping.
+        use_complex_mutation = False  # default value of whether to use extreme mutations.
 
         final_run = False
 
         simple_pool = Pool(self._SIMPLE_POOL_SIZE)
         complex_pool = Pool(self._COMPLEX_POOL_SIZE)
 
-        last_update = -1
+        previous_changes = -1
 
-        generation_index = 0
+        loop_index = 0
         offspring = 1  # Record number of generations.
         changes = 0  # Record number of total changes made.
         # (some generations may have failed mutations that do not affect the sequence.)
 
-        top_image = self.genome.render_raw_image()
-        top_score = self.calculate_error(top_image, self.base_image)
+        best_image = self.genome.render_raw_image()
+        best_error = self.calculate_error(best_image, self.base_image)
 
         for callback in callbacks:
-            callback(offspring, changes, generation_index, total_generation_changes, top_score,
-                     extreme_mutation, top_image, self.genome.adjusters)
+            callback(offspring, changes, loop_index, num_mutation_type_switches, best_error,
+                     use_complex_mutation, best_image, self.genome.adjusters)
 
         while changes < num_generations:
-            if extreme_mutation:
+            if use_complex_mutation:
                 final_run = False
 
                 offspring += self._COMPLEX_POOL_SIZE
@@ -127,46 +125,45 @@ class Evolver:
                 results = complex_pool.starmap(complex_mutation, [
                     (self.ancestor_image, self.base_image, self.sequence, self.num_circles, self.calculate_error,
                      self.palette, self.draw) for _ in range(self._COMPLEX_POOL_SIZE)])
-                score_list = [results[index][0] for index in range(self._COMPLEX_POOL_SIZE)]
-                best_index = score_list.index(min(score_list))
-                score = results[best_index][0]
+                error_list = [results[index][0] for index in range(self._COMPLEX_POOL_SIZE)]
+                best_index = error_list.index(min(error_list))
+                error = results[best_index][0]
                 mutated_gene = results[best_index][1]
                 image = results[best_index][2]
                 mutated_index = results[best_index][3]
-                top = results[best_index][4]
+                is_top_mutation = results[best_index][4]
 
                 # If successful...
-                if score < top_score:
-                    top_score = score
-                    top_image = image
+                if error < best_error:
+                    best_error = error
+                    best_image = image
                     del self.sequence[mutated_index]
-                    if top:
+                    if is_top_mutation:
                         self.sequence.append(mutated_gene)
                     else:
                         self.sequence.insert(mutated_index, mutated_gene)
                     changes += 1
                     # Reset all values and go back to regular mutations.
-                    generations_since_last_change = 0
-                    consecutive_extreme_mutations += 1
-                    if consecutive_extreme_mutations >= total_generation_changes ** 2:
+                    num_consecutive_failed_loops = 0
+                    num_complex_mutation_successes_since_switch += 1
+                    if num_complex_mutation_successes_since_switch >= num_mutation_type_switches ** 2:
                         recall_from_cache = False
-                        extreme_mutation = False
+                        use_complex_mutation = False
                 else:
-                    generations_since_last_change += 1
+                    num_consecutive_failed_loops += 1
                     # If we really can't get anywhere, then quit.
-                    if generations_since_last_change >= self._EARLY_STOPPING_LIMIT_EXTREME:
+                    if num_consecutive_failed_loops >= self._EARLY_STOPPING_LIMIT_EXTREME:
                         final_run = True
-                        generations_since_last_change = 0
+                        num_consecutive_failed_loops = 0
                         recall_from_cache = False
-                        extreme_mutation = False
+                        use_complex_mutation = False
 
             else:
-
                 offspring += self._SIMPLE_POOL_SIZE
 
                 # add the rest of the circles normally
                 if recall_from_cache:
-                    image = cached_image.copy()  # Take built image from
+                    image = cached_image.copy()  # Take built image
                 else:
                     image = self.ancestor_image.copy()
                     for index, gene in enumerate(self.sequence):
@@ -179,41 +176,41 @@ class Evolver:
                                               [(image, self.base_image, self.sequence, self.calculate_error,
                                                 self.palette, self.draw) for _ in range(self._SIMPLE_POOL_SIZE)])
 
-                score_list = [results[index][0] for index in range(self._SIMPLE_POOL_SIZE)]
-                best_index = score_list.index(min(score_list))
-                score = results[best_index][0]
+                error_list = [results[index][0] for index in range(self._SIMPLE_POOL_SIZE)]
+                best_index = error_list.index(min(error_list))
+                error = results[best_index][0]
                 mutated_gene = results[best_index][1]
                 image = results[best_index][2]
 
                 # if it was beneficial...
-                if score < top_score:
-                    top_score = score
-                    top_image = image
+                if error < best_error:
+                    best_error = error
+                    best_image = image
 
                     del self.sequence[0]
                     self.sequence.append(mutated_gene)  # Place the gene on top of the sequence again.
 
                     changes += 1  # record a change
-                    generations_since_last_change = 0
+                    num_consecutive_failed_loops = 0
                     recall_from_cache = False  # Build next image from scratch.
                     final_run = False
                 else:
-                    generations_since_last_change += 1
-                    if generations_since_last_change >= self._EARLY_STOPPING_LIMIT:
-                        total_generation_changes += 1
-                        extreme_mutation = True
-                        generations_since_last_change = 0
-                        consecutive_extreme_mutations = 0
+                    num_consecutive_failed_loops += 1
+                    if num_consecutive_failed_loops >= self._EARLY_STOPPING_LIMIT:
+                        num_mutation_type_switches += 1
+                        use_complex_mutation = True
+                        num_consecutive_failed_loops = 0
+                        num_complex_mutation_successes_since_switch = 0
                         if final_run:
                             break
 
-            generation_index += 1
+            loop_index += 1
 
-            if last_update != changes:
-                last_update = changes
+            if previous_changes != changes:
+                previous_changes = changes
                 for callback in callbacks:
-                    callback(offspring, changes, generation_index, total_generation_changes, top_score,
-                             extreme_mutation, top_image, self.genome.adjusters)
+                    callback(offspring, changes, loop_index, num_mutation_type_switches, best_error,
+                             use_complex_mutation, best_image, self.genome.adjusters)
 
         simple_pool.close()
         simple_pool.join()
