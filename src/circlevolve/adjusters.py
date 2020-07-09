c4ef8c496df86cd2cc9e4ab9642848ce1d9b8f8e
richmondvan <evan.ty.zheng@gmail.com>
2020-07-08 22:26:18 -0400

    - Refactor some parameters in dark adjuster

--- src/circlevolve/adjusters.py
+++ src/circlevolve/adjusters.py
@@ -29,26 +29,29 @@ def unadjust(adjusted_image, adjusters):
     return original_image
 
 
+DARK_ADJUST_PARAMETERS = [17.1976, 256, 512, -262.665]
+
+
 def dark_adjust(original_image):
-    adjust_parameters = [17.1976, 256, 512, -262.665]
+    global DARK_ADJUST_PARAMETERS
 
     adjusted_image = original_image.copy()
-    adjusted_image = adjusted_image + adjust_parameters[0]
-    adjusted_image = np.log(adjusted_image) / np.log(adjust_parameters[1])
-    adjusted_image = adjusted_image * adjust_parameters[2]
-    adjusted_image = adjusted_image + adjust_parameters[3]
+    adjusted_image = adjusted_image + DARK_ADJUST_PARAMETERS[0]
+    adjusted_image = np.log(adjusted_image) / np.log(DARK_ADJUST_PARAMETERS[1])
+    adjusted_image = adjusted_image * DARK_ADJUST_PARAMETERS[2]
+    adjusted_image = adjusted_image + DARK_ADJUST_PARAMETERS[3]
     adjusted_image = np.rint(adjusted_image).astype(np.uint8)
     return adjusted_image
 
 
 def dark_unadjust(adjusted_image):
-    adjust_parameters = [17.1976, 256, 512, -262.665]
+    global DARK_ADJUST_PARAMETERS
 
     original_image = adjusted_image.copy()
-    original_image = original_image - adjust_parameters[3]
-    original_image = original_image / adjust_parameters[2]
-    original_image = np.exp2(log2(adjust_parameters[1]) * original_image)
-    original_image = original_image - adjust_parameters[0]
+    original_image = original_image - DARK_ADJUST_PARAMETERS[3]
+    original_image = original_image / DARK_ADJUST_PARAMETERS[2]
+    original_image = np.exp2(log2(DARK_ADJUST_PARAMETERS[1]) * original_image)
+    original_image = original_image - DARK_ADJUST_PARAMETERS[0]
     original_image = np.rint(original_image).astype(np.uint8)
     return original_image
 
