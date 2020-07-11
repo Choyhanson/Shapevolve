8eb059453c541d0b8274f5a67d76641b1236ed3c
richmondvan <evan.ty.zheng@gmail.com>
2020-07-11 19:22:55 -0400

    - Add support for PyInstaller

--- shapevolve/__main__.py
+++ shapevolve/__main__.py
@@ -2,8 +2,15 @@
 
 import os
 import sys
+import warnings
+from multiprocessing import freeze_support
 
 if __name__ == '__main__':
+    # Handle windows multiprocessing errors.
+    freeze_support()
+    # Remove automatic deprecation warning for matplotlib
+    warnings.filterwarnings("ignore", "(?s).*MATPLOTLIBDATA.*", category=UserWarning)
+    # Add library to system path.
     sys.path.append(os.path.abspath("../"))
 
 from main import main
