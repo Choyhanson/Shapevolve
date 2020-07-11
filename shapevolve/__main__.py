87351b5121dee1ab28cc81e23099ecb7fd8401ba
richmondvan <evan.ty.zheng@gmail.com>
2020-07-11 16:13:01 -0400

    - Fixed CLI error

--- shapevolve/__main__.py
+++ shapevolve/__main__.py
@@ -3,9 +3,11 @@
 import os
 import sys
 
+if __name__ == '__main__':
+    sys.path.append(os.path.abspath("../"))
+
 from main import main
 
 if __name__ == '__main__':
-    sys.path.append(os.path.abspath("../"))
     main()
 
