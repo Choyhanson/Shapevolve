88640f86ea686a0368145f0b794166dc637a91b8
richmondvan <evan.ty.zheng@gmail.com>
2020-07-12 18:03:08 -0400

    Updated version num

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Evan Zheng'
 author = 'Evan Zheng'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.1.2-alpha'
+release = 'v0.1.3-alpha'
 
 
 # -- General configuration ---------------------------------------------------
